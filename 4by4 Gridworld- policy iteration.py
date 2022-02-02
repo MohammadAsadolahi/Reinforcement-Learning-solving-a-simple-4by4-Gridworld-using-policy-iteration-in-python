8408f0b817dd196e568cd25882763a155f4fd08d
Mohammad Asadolahi <79268727+mohammadAsadolahi@users.noreply.github.com>
2022-02-02 04:55:00 -0800

    whole algorithm updated

--- 4by4 Gridworld- policy iteration.py
+++ 4by4 Gridworld- policy iteration.py
@@ -2,16 +2,13 @@
 # WRITTEN BY MOHAMMAD ASADOLAHI
 # Mohammad.E.Asadolahi@gmail.com
 # https://github.com/mohammadAsadolahi
-
 import numpy as np
-
 class GridWorld:
     def __init__(self):
         # S O O O
         # O O O *
         # O * O O
         # O * 0 T
-        self.currentState = None
         self.actionSpace = ('U', 'D', 'L', 'R')
         self.actions = {
             (0, 0): ('D', 'R'),
@@ -30,7 +27,7 @@ class GridWorld:
             (3, 1): ('U', 'L', 'R'),
             (3, 2): ('U', 'L', 'R')
         }
-        self.rewards = {(3, 3): 5, (1, 3): -2, (2, 1): -2, (3, 1): -10}
+        self.rewards = {(3, 3): 5, (1, 3): -2, (2, 1): -2, (3, 1): -2}
         self.explored = 0
         self.exploited = 0
 
@@ -40,13 +37,25 @@ class GridWorld:
             policy[state] = np.random.choice(self.actions[state])
         return policy
 
-    def getCurrentState(self):
-        if not self.currentState:
-            self.currentState = (0, 0)
-        return self.currentState
-
+    def reset(self):
+        return (0, 0)
+        
     def is_terminal(self, s):
         return s not in self.actions
+
+    def getNewState(self,state,action):
+      i, j = zip(state)
+      row = int(i[0])
+      column = int(j[0])
+      if action == 'U':
+          row -= 1
+      elif action == 'D':
+          row += 1
+      elif action == 'L':
+          column -= 1
+      elif action == 'R':
+          column += 1
+      return row,column
 
     def chooseAction(self, state, policy, exploreRate):
         if exploreRate > np.random.rand():
@@ -58,52 +67,15 @@ class GridWorld:
     def greedyChoose(self, state, values):
         actions = self.actions[state]
         stateValues = []
-        for item in actions:
-            i, j = zip(state)
-            row = int(i[0])
-            column = int(j[0])
-            if item == 'U':
-                row -= 1
-            elif item == 'D':
-                row += 1
-            elif item == 'L':
-                column -= 1
-            elif item == 'R':
-                column += 1
+        for act in actions:
+            row,column=self.getNewState(state,act)
             if (row, column) in values:
                 stateValues.append(values[(row, column)])
         return actions[np.argmax(stateValues)]
-
-    def getActionReward(self, state, action):
-        i, j = zip(state)
-        row = int(i[0])
-        column = int(j[0])
-        if action == 'U':
-            row -= 1
-        elif action == 'D':
-            row += 1
-        elif action == 'L':
-            column -= 1
-        elif action == 'R':
-            column += 1
-        if (row, column) in self.rewards:
-            return self.rewards[(row, column)]
-        else:
-            return 0
-
+    
     def move(self, state, policy, exploreRate):
         action = self.chooseAction(state, policy, exploreRate)
-        i, j = zip(state)
-        row = int(i[0])
-        column = int(j[0])
-        if action == 'U':
-            row -= 1
-        elif action == 'D':
-            row += 1
-        elif action == 'L':
-            column -= 1
-        elif action == 'R':
-            column += 1
+        row,column=self.getNewState(state,action)
         if (row, column) in self.rewards:
             return (row, column),self.rewards[(row, column)]
         return (row, column), 0
@@ -137,29 +109,32 @@ class GridWorld:
         print("----------------------------")
         
         
- 
+        
 enviroment = GridWorld()
 policy = enviroment.getRandomPolicy()
+# enviroment.printPolicy(policy)
+
 #example optimal policy = {(0, 0): 'R', (0, 1): 'R', (0, 2): 'D', (0, 3): 'D', (1, 0): 'R', (1, 1): 'D', (1, 2): 'D', (1, 3): 'D',
 #           (2, 0): 'R', (2, 1): 'D', (2, 2): 'R', (2, 3): 'D', (3, 0): 'R', (3, 1): 'R', (3, 2): 'R'}
 
-enviroment.printPolicy(policy)
-state = enviroment.getCurrentState()
-values = {}
+for i in range(10):
+  values = {}
+  for state in policy:
+      values[state] = 0
+  values[(3, 3)] = 5
 
-for j in range(10):
-    values.clear()
-    for state in policy:
-        values[state] = 0
-    values[(3, 3)] = 5
-    for i in range(10):
-        for item in enviroment.actions.keys():
-            nextState, reward = enviroment.move(item, policy, exploreRate=0.2)
-            values[item] = reward + 0.1 * values[nextState]
-        for item in policy:
-             policy[item] = enviroment.greedyChoose(item, values)
-    if (j%1)==0:
-      print(f"\n\n\n step:{j}")
-      enviroment.printVaues(values)
-      enviroment.printPolicy(policy)
+  for j in range(10):
+    state = enviroment.reset()
+    while not enviroment.is_terminal(state):# and can add for example terminal condition after 20steps
+      nextState, reward = enviroment.move(state, policy, exploreRate=0.05)
+      values[state] = reward + 0.1 * values[nextState]
+      state=nextState
+  for item in policy:
+        policy[item] = enviroment.greedyChoose(item, values)
+  
+  if (i%1)==0:
+    print(f"\n\n\n step:{i}")
+    # enviroment.printVaues(values)
+    enviroment.printPolicy(policy)
+
 print(f"exploited:{enviroment.exploited}  explored:{enviroment.explored}")
